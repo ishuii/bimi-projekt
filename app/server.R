51709240d31b625b9511f00a61d5edea0931059d
ishuii <wiktoria.d.cholewa@gmail.com>
2026-04-04 11:57:49 +0200

    Change comments to English

--- app/server.R
+++ app/server.R
@@ -1,21 +1,24 @@
-# Server-Logik
+# Server logic
+
 library(shiny)
-options(shiny.maxRequestSize = 100 * 1024^2) # Erhöhe Uploadlimit
+
+options(shiny.maxRequestSize = 100 * 1024^2) # increase upload limit
+
 #Server ---------------------
-server<- function(input, output) {
+server <- function(input, output) {
   output$Beispieltext <- renderText({
     paste("Deine Zahl:", input$x)
   })
   
   
   # CSV IMPORT BACKEND
-  daten <- reactive({      #Für dynamische Verarbeitung
-    req(input$Datei_csv) # Check hochgeladen
+  daten <- reactive({      # for dynamic processing
+    req(input$Datei_csv)   # upload check
     read.csv(input$Datei_csv$datapath)
     
   })
-  #CSV DATENVERARBEITUNG
   
+  #CSV PROCESSING
   output$Spalten <- renderText({
     paste0("Der Datensatz hat ",nrow(daten())-1, " Messwerte", "\n",
    "Der Datensatz hat ",ncol(daten()), " Samples")
