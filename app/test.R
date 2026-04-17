74a846e22d22f273cf39b9520e5e30de3a363407
adrii2604 <adrinaras24@gmail.com>
2026-04-17 21:36:17 +0200

    Upgedated Code

--- app/test.R
+++ app/test.R
@@ -13,12 +13,26 @@ main <- dashboardPage(skin = "yellow",
       sidebarMenu(id = "tabs",
         menuItem("Startseite", tabName = "Startseite", icon = icon("home")),
         menuItem("Datei Hochladen", icon = icon("upload"), tabName = "datei_hochladen"),
-        menuItem("Parametern Wählen", icon = icon("sliders"), tabName = "parameter")
+        menuItem("Parametern Wählen", icon = icon("sliders"), tabName = "parameter"),
+        menuItem("Heatmap", icon = icon("bar"), tabName = "heatmap")
+        
         
         )
       ),
     
     dashboardBody(
+      
+      tags$style(HTML("
+        .form-control{
+          font-size: 16px;
+        }
+        
+        label{
+        font-size: 16px;
+        font-weight:600;
+        }
+      ")),
+      
       tabItems(
         
         tabItem(tabName = "Startseite",
@@ -41,47 +55,96 @@ main <- dashboardPage(skin = "yellow",
                        verbatimTextOutput("Spalten")
                        )
                      ),
-                actionButton('switchtab', 'Hochladen')
+
+                fluidRow(
+                  
+                  box(
+                    title = "Datensatz Parametern einstellen",
+                    width = 12,
+                    status = "success",
+                    
+                    checkboxGroupInput(inputId = "pathways", label = "Pathways Auswählen",
+                                       choices = list("Choice 1" = 1, "Choice 2" = 2, "Choice 3" = 3),
+                                       selected = 1, width = '400px', inline = FALSE),
+                    
+                    checkboxGroupInput(inputId = "gene", label = "Gene Auswählen",
+                                       choices = list("Choice 1" = 1, "Choice 2" = 2, "Choice 3" = 3),
+                                       selected = 1, width = '400px', inline = FALSE)
+                
+                  )
+                ),
+                
+                actionButton('switchtab', 'Parametern Wählen'),
+                
         ),
         
         tabItem(tabName = "parameter",
                 h2("Bitte Parametern benötigt zur Cluster Analyse, auswählen"),
-                selectInput(inputId = "clusterverfahren", label = "Clusterverfahren auswählen", 
-                            choices = c("Single-Linkage", "Average-Linkage", "Complete-Linkage")),
                 
-                
-                radioButtons(inputId = "farbpaletten", label = "Farbpalette für Heatmaps auswählen", 
-                             choices = list(
-                               "Option 1" = 1,
-                               "Option 2" = 2,
-                               "Option 3" = 3
-                             )
-                             ),
-                
-                numericInput(inputId = "azahlcluster", label = "Anzahl von Clustern eingeben",
-                             value = 2, min = 1, max = 10),
-                
-                selectInput(inputId = "normalisierung", label = "Normalisierungs Verfahren auswählen", 
-                            choices = c("a", "b", "c")),
-                
-                selectInput(inputId = "distanzmatrix", label = "Distanz Matrix auswählen", 
-                            choices = c("Euklidische distanz", "b", "c")),
-                
-                checkboxGroupInput(inputId = "variable", label = "Variablen Auswählen",
-                                   choices = list("Choice 1" = 1, "Choice 2" = 2, "Choice 3" = 3),
-                                   selected = 1, width = '400px', inline = FALSE),
-                
-                radioButtons(inputId = "gewebe", label = "Gewebe art auswählen", 
-                             choices = list(
-                               "Gesunde Gewebe" = 1,
-                               "Ungesunde Gewebe" = 2
-                             )
+                fluidRow(
+                  
+                  box(
+                    title = "Cluster Einstellungen",
+                    width = 6,
+                    solidHeader = TRUE,
+                    status = "primary",
+                    
+                    selectInput(inputId = "clusterverfahren", label = "Clusterverfahren auswählen", 
+                                choices = c("Single-Linkage", "Average-Linkage", "Complete-Linkage")),
+                    
+                    radioButtons(inputId = "farbpaletten", label = "Farbpalette für Heatmaps auswählen", 
+                                 choices = list(
+                                   "Option 1" = 1,
+                                   "Option 2" = 2,
+                                   "Option 3" = 3
+                                 )
+                    ),
+                    
+                    numericInput(inputId = "anzahlcluster", label = "Anzahl von Clustern eingeben",
+                                 value = 2, min = 1, max = 10),
+                    
+                    
+                    selectInput(inputId = "normalisierung", label = "Normalisierungs Verfahren auswählen", 
+                                choices = c("a", "b", "c")),
+                    
+                    
+                    selectInput(inputId = "distanzmatrix", label = "Distanz Matrix auswählen", 
+                                choices = c("Euklidische distanz", "b", "c"))
+                    
+                  ),
+                  
+                  box(
+                    title = "Datensatz Einstellungen",
+                    width = 6,
+                    status = "primary",
+                    solidHeader = TRUE,
+                    
+                    checkboxGroupInput(inputId = "variable", label = "Variablen Auswählen",
+                                       choices = list("Choice 1" = 1, "Choice 2" = 2, "Choice 3" = 3),
+                                       selected = 1, width = '400px', inline = FALSE),
+                    
+                    
+                    radioButtons(inputId = "gewebe", label = "Gewebe art auswählen", 
+                                 choices = list(
+                                   "Gesunde Gewebe" = 1,
+                                   "Ungesunde Gewebe" = 2
+                                 )
+                    ),
+                    
+                    checkboxGroupInput(inputId = "koerper", label = "Körperteile Auswählen",
+                                       choices = list("Choice 1" = 1, "Choice 2" = 2, "Choice 3" = 3),
+                                       selected = 1, width = '100px', inline = FALSE),
+                    
+                  )
                 ),
                 
-                checkboxGroupInput(inputId = "koerper", label = "Körperteile Auswählen",
-                                   choices = list("Choice 1" = 1, "Choice 2" = 2, "Choice 3" = 3),
-                                   selected = 1, width = '100px', inline = FALSE),
-                )
+                actionButton('run', 'Run Cluster Analyse'),
+                
+                ),
+        
+        
+        tabItem(tabName = "heatmap",
+                h2("Heatmap"))
       )
     )
  )
@@ -115,6 +178,10 @@ server <- function(input, output, session) {
    updateTabItems(session, "tabs", selected = "parameter")
   })
   
+  observeEvent(input$run, {
+    updateTabItems(session, "tabs", selected = "heatmap")
+  })
+  
 }
   
 shinyApp(main, server)
@@ -123,4 +190,4 @@ shinyApp(main, server)
 
 
 
-?checkboxGroupInput      
+?valueBox
