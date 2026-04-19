e12be355bb11d77ae382e527e39866c94daf5a60
ishuii <wiktoria.d.cholewa@gmail.com>
2026-04-19 11:46:03 +0200

    update distance function

--- R/clustering/distance_matrix.R
+++ R/clustering/distance_matrix.R
@@ -1,12 +1,48 @@
-# in case we are not allowed to use the dist function from stats
+library(Rcpp)
 
-dist <- function(data_set, method = "euclidean") {
-  n <- nrow(data_set)
+cppFunction('
+  #include <cmath>
+  using namespace Rcpp;
+  
+  // [[Rcpp::export]]
+  NumericMatrix dist_cpp(NumericMatrix mat, std::string method) {
+    int n = mat.nrow();
+    int p = mat.ncol();
+    NumericMatrix dmat(n, n);
+      
+    for (int i = 0; i < n; i++) {
+        for (int j = i; j < n; j++) {
+            double d = 0.0;
+              
+            for (int k = 0; k < p; k++) {
+                double diff = mat(i,k) - mat(j,k);
+                  
+                if (method == "euclidean") {
+                  d += diff*diff;
+                } else if (method == "manhattan") {
+                  d += std::abs(diff); 
+                }
+            }
+            
+            if (method == "euclidean") {
+              d = std::sqrt(d);
+            }
+            
+            dmat(i,j) = d;
+            dmat(j,i) = d;
+        }
+    }
+    return dmat;
+  }')
+
+# old version, just here for testing
+basic_dist <- function(df, method = "euclidean") {
+  n <- nrow(df)
   mat <- matrix(0, n, n) # create empty matrix
   
   for (i in 1:n) {
     for (j in i:n) {
-      diff <- data_set[i,] - data_set[j,] # row-wise comparison 
+      diff <- df[i,] - df[j,] # row-wise comparison 
       
       if (method == "euclidean") {
         d <- sqrt(sum(diff^2))
@@ -24,17 +60,3 @@ dist <- function(data_set, method = "euclidean") {
   }
   return(mat)
 }
-
-
-# Testing ----------------------------------------------------------
-
-df <- read.csv("data/TCGA_kidney_unnormalized_TOP10.csv", row.names = 1)
-
-labels <- df[c(11),]
-df <- df[-c(11),]
-
-df_log <- log2(df + 1)
-df_normalized <- t(scale(t(df_log)))
-
-
-dist_mat <- dist(t(df_normalized), method = "euclidean")
---
+++ tests/distance_function_tests.R
@@ -0,0 +1,27 @@
+# Testing: Distance functions
+
+source("R/clustering/distance_matrix.R")
+library(microbenchmark)
+library(stats)
+
+df <- read.csv("data/TCGA_kidney_unnormalized_TOP10.csv", row.names = 1)
+
+labels <- df[c(11),]
+df <- df[-c(11),]
+
+df_log <- log2(df + 1)
+df_normalized <- t(scale(t(df_log)))
+
+# we benchmark with a smaller distance matrix
+microbenchmark(
+  base = basic_dist(df_normalized, method = "euclidean"), 
+  stats = stats::dist(df_normalized, method = "euclidean"),
+  cpp = dist_cpp(df_normalized, method = "euclidean")
+)
+
+# results
+# Unit: microseconds
+#   expr    min      lq     mean  median     uq     max neval
+#   base 1364.4 1383.05 2130.921 1425.95 1852.7 14920.2   100
+#  stats  201.8  206.40  233.314  220.45  249.6   344.9   100
+#    cpp  168.1  169.50  196.110  171.80  176.4  2043.1   100
