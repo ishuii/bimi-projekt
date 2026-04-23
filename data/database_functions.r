e8f6d95d7864024797580957754fd2c3856a3e41
AlisaPeste <pesteritzalisa@gmail.com>
2026-04-23 21:12:49 +0200

    Update database functions and JF

--- data/database_functions.r
+++ data/database_functions.r
@@ -1,6 +1,7 @@
 library(RSQLite)
 library(DBI)
 
+#Connetion object
 con <- dbConnect(RSQLite::SQLite(), "GeneDatabase.sqlite")
 
 #---------------------------------------
@@ -12,27 +13,28 @@ dataset <- read.csv("data/TCGA_kidney_unnormalized.csv")
 #Genes cannot be originally used as rownames as they can appear more than once
 preprocess_dataset <- function(data) {
 
-    data_labels_index <- nrow(data)
-    data_labels <- data[data_labels_index, ]
+  data_labels_index <- nrow(data)
+  data_labels <- data[data_labels_index, ]
 
-    data_withoutlabels <- data[-data_labels_index, ]
+  data_withoutlabels <- data[-data_labels_index, ]
 
 
-    #rename first column as Entrez_ID 
-    #== corresponds to the name of the column in the database
-    colnames(data_withoutlabels)[1] <- "Entrez_ID"
+  #rename first column as Entrez_ID 
+  #== corresponds to the name of the column in the database
+  colnames(data_withoutlabels)[1] <- "Entrez_ID"
 
-    return(list(data_withoutlabels, data_labels))
+  return(list(data_withoutlabels, data_labels))
 
 }
 
-result <- preprocess_dataset(dataset)
+#--------------------------------
+#Test function preprocess_dataset
+#-------------------------------
 
+result <- preprocess_dataset(dataset)
 
 dataset_processed <- result[[1]]
 dataset_processed_labels <- result[[2]]
-
-
 
 
 #--------------------------------------------------------------------
@@ -40,15 +42,17 @@ dataset_processed_labels <- result[[2]]
 #--------------------------------------------------------------------
 get_column_from_database <- function(con, tablename, columname) {
 
-  query <- paste0("SELECT ", columname, " FROM ", tablename)
+  query <- paste0("SELECT ", columname, " FROM ", tablename, " Order by ",columname, " ASC" )
 
   columns <- dbGetQuery(con, query)
 
   return(columns)
 
 }
-
+#-------------
+#Test function 
 #Use for GUI
+#-------------
 columns <- get_column_from_database(con,"Pathway", "Name")
 pathway_name <- columns$Name
 
@@ -56,7 +60,7 @@ pathway_name <- columns$Name
 #Function for one chosen pathway
 #get all genes which correspond to the pathway
 #-------------------------------------------------
-get_genes_for_chosen_pathway <- function(userChoice,con){
+get_genes_for_pathway <- function(userChoice,con){
 
 query <- "SELECT l.Entrez_ID, p.Name FROM Pathway AS p, Lookup_Gene_Pathway AS l 
           WHERE p.Pathway_ID = l.Pathway_ID 
@@ -64,12 +68,50 @@ query <- "SELECT l.Entrez_ID, p.Name FROM Pathway AS p, Lookup_Gene_Pathway AS l
 
 result <- dbGetQuery(conn = con, query, params= userChoice)
 
-return(result)
+return(result$Entrez_ID)
 
 
 }
 
-entrez_ids <- get_genes_for_chosen_pathway("Metabolic pathways", con = con)
+#-------------
+#test function 
+#-------------
+entrez_ids <- get_genes_for_pathway("Fatty acid metabolism", con = con)
+
+
+#------------------------------------------------
+#Function for more than one chosen pathway
+#get all genes which correspond to the pathways
+#-------------------------------------------------
+get_genes_for_pathways <- function(vector_userchoices,con){
+
+resultvec <- c()
+
+for (i in 1:length(vector_userchoices)){
+    query <- "SELECT l.Entrez_ID, p.Name FROM Pathway AS p, Lookup_Gene_Pathway AS l 
+            WHERE p.Pathway_ID = l.Pathway_ID 
+            AND p.Name = ?"
+
+    result <- dbGetQuery(conn = con, query, params= vector_userchoices[i])
+
+    resultvec <- c(resultvec,result$Entrez_ID)
+
+
+
+}
+
+return(unique(resultvec))
+
+}
+
+#---------------
+#test function 
+#-----------------
+
+userinput <- c("Fatty acid metabolism", "Biosynthesis of nucleotide sugars")
+entrez_ids_2 <- get_genes_for_pathways(userinput, con)
+
+
 
 
 #----------------------------------------------------
@@ -79,11 +121,17 @@ entrez_ids <- get_genes_for_chosen_pathway("Metabolic pathways", con = con)
 extract_relevant_genes <- function(extracted_genes, original_data){
 
 
-extracted_dataset <- original_data[original_data$Entrez_ID %in% extracted_genes$Entrez_ID, ]
+extracted_dataset <- original_data[original_data$Entrez_ID %in% extracted_genes, ]
 
 return(extracted_dataset)
 
 }
 
-extracted_dataset <- extract_relevant_genes(entrez_ids, dataset_processed)
+#--------------
+#test function 
+#-------------
 
+extracted_dataset <- extract_relevant_genes(entrez_ids_2, dataset_processed)
+
+#aggregate if there are duplicates 
+#final_exctracted_dataset <- aggregate(. ~ Entrez_ID, data = extracted_dataset, FUN = mean)
