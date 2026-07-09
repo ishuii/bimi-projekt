f7a7062bac9953548caf3c50b85d9d8e195bf8cc
RoRo <rosina.roeckseisen@gmail.com>
2026-07-09 10:45:23 +0200

    added one norm method

--- tests/normalization_function_tests.R
+++ tests/normalization_function_tests.R
@@ -1,4 +1,4 @@
-# Function testing
+
 library(ggplot2)
 library(distRcpp)
 library(RSQLite)
@@ -7,14 +7,19 @@ library(reshape2)
 library(viridis) 
 library(RColorBrewer) 
 
+# ============================================================
+# SOURCE FILES
+# ============================================================
+
 source("data/database_functions_v4.r")
 source("R/clustering/normalization_methods.R")
 source("R/clustering/prepare_data.R")
 source("R/clustering/hierarchical_clustering.R")
-source("R/visualization/tree_functions.R") 
-source("R/visualization/dendro_functions_V2.R") 
-source("R/visualization/dendro_functions.R")
-source("R/visualization/heatmap.R")
+source("R/visualization/heatmap_final.R")
+source("R/visualization/final_dendrogram_functions.R")
+source("R/visualization/final_dendrogram.R")
+source("R/visualization/final_tree_functions.R")
+source("R/visualization/Grafikpanel.R")
 
 
 # ------------------------------ TESTING ------------------------------------------
@@ -197,22 +202,28 @@ corr_df <- get_correlation_distance(prepared_data)
 ####################################################################################
 
 ####################################################################################
-#test NaN-erzeugung
+
+
+# ============================================================
+# DATENBANK und DATENSATZ
+# ============================================================
 
 con <- dbConnect(RSQLite::SQLite(), "GeneDatabase.sqlite")
 
 # Anmerkung: Datensatz immer unter data/... ablegen, damit dieser Aufruf für alle reproduzierbar ist!
-dataset_kidney_meta <- read.csv("data/colon_vs_pancreas_meta.csv", header = TRUE)
+dataset_golub <- read.csv("data/GOLUB_microarray.csv", header = TRUE)
+dataset_kidney <- read.csv("data/TCGA_kidney_unnormalized_meta.csv")
+dataset_ship <- read.csv("data/SHIPP_microarray.csv")
 
 # ============================================================
 # PATHWAYS und INTEGRATION
 # ============================================================
 
 # diese Auswahl ist hier hart codiert, wird normalerweise in der GUI ausgewählt
-meine_pathways <- c("Biosynthesis of amino acids")
+meine_pathways <- c("Pathways in cancer")
 message("Nutze Pathway für den Nieren-Test: ", meine_pathways)
 
-preprocess        <- preprocess_general(dataset_kidney_meta)
+preprocess        <- preprocess_general(dataset_ship)
 data_preprocessed <- preprocess$dataset_preprocessed
 
 result <- run_data_integration(
@@ -224,22 +235,70 @@ result <- run_data_integration(
 gefilteterDatensatz <- result$filtered_dataset
 metaDaten_gefiltert <- result$meta_data
 
-dbDisconnect(con)
+# dbDisconnect(con)
 
 # ============================================================
 # PREPARE DATA UND NORMALISIERUNG
 # ============================================================
 
 df_prepared   <- prepare_data(gefilteterDatensatz)
-#summary(df_prepared)
-#min(df_prepared, na.rm = TRUE)
-#dim(df_prepared)
-#sum(df_prepared < -1, na.rm = TRUE)
-#which(df_prepared < -1, arr.ind = TRUE)
-
-df_normalized <- normalization(df_prepared, 4)
+df_normalized <- normalization(df_prepared, 5)
 
 patient_names <- colnames(df_normalized)
 class_labels  <- as.character(metaDaten_gefiltert["Meta_labels", patient_names])
 
+# ============================================================
+# DISTANZMATRIX UND CLUSTERING
+# ============================================================
 
+# PATIENTEN
+dist_mat_pat <- dist_cpp(t(df_normalized), "euclidean")
+cluster_pat  <- hierarchical_clustering(dist_mat_pat, "single")
+
+# GENE
+dist_mat_genes <- dist_cpp(df_normalized, "euclidean")
+cluster_genes  <- hierarchical_clustering(dist_mat_genes, "single")
+
+# ============================================================
+# DENDROGRAMM
+# ============================================================
+
+# PATIENTEN
+tree_pat   <- build_tree(cluster_pat)
+order_pat  <- get_order_vector(tree_pat)
+patient_dendro <- generate_dendro(cluster_pat, tree_pat, order_pat, title="Patienten", names_vector=patient_names, class_labels=class_labels)
+
+# GENE
+tree_genes   <- build_tree(cluster_genes)
+order_genes  <- get_order_vector(tree_genes)
+gene_dendro <- generate_dendro(cluster_genes, tree_genes, order_genes, title="Gene", names_vector=NULL, class_labels=NULL)
+
+# ============================================================
+# HEATMAP
+# ============================================================
+
+viridis <- viridis::viridis(100) 
+RdYlBu <- brewer.pal(11, "RdYlBu") 
+RdBu <- brewer.pal(11, "RdBu") 
+PRGn <- brewer.pal(11, "PRGn") 
+
+heatmap1 <- generate_heatmap(df_normalized, order_genes, order_pat)
+grafikpanel(heatmap1, patient_dendro, gene_dendro)
+
+# zum Vergleichen
+stats::heatmap(df_normalized)
+
+##########################################################################################
+
+##########################################################################################
+
+library(pheatmap)
+
+pheatmap(
+  df_prepared,
+  cluster_rows = TRUE,      # Gene clustern
+  cluster_cols = TRUE,      # Patienten clustern
+  show_rownames = FALSE,
+  show_colnames = TRUE,
+  color = colorRampPalette(c("blue", "white", "red"))(100)
+)
